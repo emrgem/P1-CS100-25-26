969ef347d4dd8b52ea436c69ec89fdd2ab8a6824
emrgem <emrgem@bergen.org>
2025-10-01 08:56:30 -0400

    While loops started

---
+++ Unit2-Control-Flow/Lesson6-While-Loops/part1-basics/while.py
@@ -0,0 +1,61 @@
+#syntax:
+'''
+initialize variable
+while condition(test variable):
+    code block
+    increment/decrement variable
+'''
+
+# Example: Count to 5
+print("=== Count to 5 ===")
+count = 1
+
+
+while count <= 5:
+    print(count)
+    count += 1
+    
+# YOUR TURN: Countdown Timer
+num = 5
+
+
+while num > 0:
+    print(num)
+    num -= 1
+   
+print("Blastoff!")
+
+# Sum numbers 1 to 10
+num = 1
+total = 0
+
+# while num <= 10:
+#     total += num #(total = total + num)
+#     num += 1
+
+# print(f"Sum of numbers 1-10: {total}")
+
+while num <= 10:
+    total += num
+    if num < 10:
+        print(num, end="+")
+    else:
+        print(num, end="=")
+    num += 1
+print(total)
+print()
+
+# Sum of digits
+# take a user input as int, and sum the digits of it
+number = input("Enter a number:") #1234
+sum = 0
+# for char in number:
+#     print(f"{char} {type(char)}")
+#     sum += int(char)
+# print(f"Total {sum}")
+
+i=0
+while i<len(number):
+    sum += int(number[i])
+    i += 1
+print(f"Total {sum}")
