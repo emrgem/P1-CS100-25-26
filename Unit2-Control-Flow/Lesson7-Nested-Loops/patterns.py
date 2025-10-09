dee2803edf917fc7954e9c0efe7a10b17050627b
emrgem <emrgem@bergen.org>
2025-10-09 09:14:15 -0400

    nested loops practice

---
+++ Unit2-Control-Flow/Lesson7-Nested-Loops/patterns.py
@@ -0,0 +1,10 @@
+# Pyramid Pattern
+
+# STEP-BY-STEP GUIDE:
+# Row 0: 4 spaces, 1 star
+# Row 1: 3 spaces, 3 stars
+# Row 2: 2 spaces, 5 stars
+# Row 3: 1 space, 7 stars
+# Row 4: 0 spaces, 9 stars
+
+rows = 5
