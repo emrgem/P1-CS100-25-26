c6e8274ec5726f041ea1ae67c8d90b461a9b7d43
emrgem <emrgem@bergen.org>
2025-10-10 08:47:18 -0400

    pyramid pattern completed

--- Unit2-Control-Flow/Lesson7-Nested-Loops/patterns.py
+++ Unit2-Control-Flow/Lesson7-Nested-Loops/patterns.py
@@ -8,3 +8,13 @@
 # Row 4: 0 spaces, 9 stars
 
 rows = 5
+# Step1:create an outer loop for the rows
+for i in range(rows):
+    #step2:print the spaces rows-i-1
+    for j in range(rows-i-1):
+        print(" ", end="")
+    #step3:print the stars 2*i+1
+    for k in range(2*i+1):
+        print("*", end="")
+    #step4:print a new line
+    print()
