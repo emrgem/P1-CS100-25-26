5792b95b84ab637dda1f3dd36c0cec07d25c7aee
emrgem <emrgem@bergen.org>
2025-11-21 08:20:55 -0500

    common exceptions

---
+++ Unit3-Functions/Lesson5-Exception-Handling/common_exceptions.py
@@ -0,0 +1,37 @@
+# 10 Common Python Exceptions Examples for Beginners (without try/except)
+
+# # 1 - ZeroDivisionError
+# print(10 / 0)
+
+# # 2 - ValueError
+# print(int("abc"))
+
+# # 3 - KeyError
+# grades = {"Bob": 90}
+# print(grades["Bill"])
+
+# # 4 - IndexError
+# numbers = [1, 2, 3]
+# print(numbers[5])
+
+# # 5 - NameError
+# print(total)
+# total = 10
+
+# # 6 - TypeError
+# print("hello" + 5)
+
+# # 7 - SyntaxError (cannot run directly, will crash)
+# for i in range(5)
+#     print(i)
+
+# # 8 - AttributeError
+# x = 5
+# x.append(3)
+
+# # 9 - ModuleNotFoundError
+# import wonka_factory
+
+# # 10 - IndentationError (cannot run directly, will crash)
+# if True:
+# print("Hi")
